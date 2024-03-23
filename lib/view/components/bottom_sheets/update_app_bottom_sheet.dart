8778b5a4c70dee1a71a9949d823a88a429e55555
sewadzhan <skhan_1@edu.hse.ru>
2024-03-24 02:21:15 +0500

    Update app bottom sheet was done

--- lib/view/components/bottom_sheets/update_app_bottom_sheet.dart
+++ lib/view/components/bottom_sheets/update_app_bottom_sheet.dart
@@ -16,67 +16,74 @@ class UpdateAppBottomSheet extends StatelessWidget {
     super.key,
   });
 
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        SafeArea(
+          child: Container(
+              decoration: const BoxDecoration(
+                  color: Constants.backgroundColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15))),
+              padding: EdgeInsets.only(
+                left: Constants.defaultPadding,
+                right: Constants.defaultPadding,
+                top: Constants.defaultPadding * 2,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(
+                          bottom: Constants.defaultPadding * 1.5),
+                      width: 150,
+                      child: SvgPicture.asset('assets/logo/logo-dark.svg')),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        bottom: Constants.defaultPadding * 0.25),
+                    child: Text(
+                      "Доступно обновление",
+                      style: Constants.textTheme.displaySmall,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.only(bottom: Constants.defaultPadding * 1.5),
+                    child: Text(
+                      "Обновите приложение для дальнейшей работы",
+                      style: Constants.textTheme.headlineMedium!
+                          .copyWith(fontWeight: FontWeight.normal),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      bottom: Constants.defaultPadding * 0.25,
+                    ),
+                    child: CustomElevatedButton(
+                        text: "Обновить",
+                        function: () {
+                          if (Platform.isAndroid) {
+                            _launchURL(playMarketUrl);
+                          } else if (Platform.isIOS) {
+                            _launchURL(appStoreUrl);
+                          }
+                        }),
+                  )
+                ],
+              )),
+        ),
+      ],
+    );
+  }
+
   void _launchURL(String str) async {
     var url = Uri.parse(str);
     await canLaunchUrl(url)
         ? await launchUrl(url)
         : throw 'Could not launch $url';
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-          decoration: const BoxDecoration(
-              color: Constants.backgroundColor,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15), topRight: Radius.circular(15))),
-          padding: EdgeInsets.only(
-            left: Constants.defaultPadding * 1.5,
-            right: Constants.defaultPadding * 1.5,
-            top: Constants.defaultPadding * 2,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                  padding:
-                      EdgeInsets.only(bottom: Constants.defaultPadding * 2),
-                  width: 150,
-                  child: SvgPicture.asset('assets/logo/logo-dark.svg')),
-              Padding(
-                padding: EdgeInsets.only(bottom: Constants.defaultPadding),
-                child: Text(
-                  "Доступно обновление",
-                  style: Constants.textTheme.displaySmall,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: Constants.defaultPadding),
-                child: Text(
-                  "Обновите приложение для дальнейшей работы",
-                  style: Constants.textTheme.displaySmall!
-                      .copyWith(fontWeight: FontWeight.normal),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  bottom: Constants.defaultPadding * 0.25,
-                ),
-                child: CustomElevatedButton(
-                    text: "Обновить",
-                    function: () {
-                      if (Platform.isAndroid) {
-                        _launchURL(playMarketUrl);
-                      } else if (Platform.isIOS) {
-                        _launchURL(appStoreUrl);
-                      }
-                    }),
-              )
-            ],
-          )),
-    );
   }
 }
